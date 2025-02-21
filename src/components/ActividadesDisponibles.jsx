007adf44d64f9aa790f80f56497f745862f1c87d
DouglasCasco <97561373+douglascasco@users.noreply.github.com>
2025-02-21 09:41:00 -0600

    Update ActividadesDisponibles.jsx

--- src/components/ActividadesDisponibles.jsx
+++ src/components/ActividadesDisponibles.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import '../styles/ActividadesDisponibles.css'; // Asegúrate de tener este archivo para los estilos
 
+
+
 const ActividadesDisponibles = ({ data }) => {
     console.log('Datos de prueba:', data);
     const [actividades] = useState(data || []);
@@ -8,19 +10,24 @@ const ActividadesDisponibles = ({ data }) => {
     return (
         <div className="actividades-container">
             <div className="actividades-list">
-                {actividades.map((actividad) => (
-                    <div key={actividad.id} className="actividad-box">
-                        <img src={actividad.imagen} alt={actividad.nombre} className="actividad-imagen" />
-                        <div className="actividad-info">
-                            <h3>{actividad.nombre}</h3>
-                            <p><strong>Organizador:</strong> {actividad.organizador}</p>
-                            <p><strong>Fecha:</strong> {actividad.fecha}</p>
+                {actividades.length === 0 ? (
+                    <p>No hay actividades disponibles</p>
+                ) : (
+                    actividades.map((actividad) => (
+                        <div key={actividad.id} className="actividad-box">
+                            <img src={actividad.imagen} alt={actividad.nombre} className="actividad-imagen" />
+                            <div className="actividad-info">
+                                <h3>{actividad.nombre}</h3>
+                                <p><strong>Organizador:</strong> {actividad.organizador}</p>
+                                <p><strong>Fecha:</strong> {actividad.fecha}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+                )}
             </div>
         </div>
     );
+    
 };
 
 export default ActividadesDisponibles;
