b41aff589fcf042f4367ca9bff3ad835a42ac4e7
DOUGLAS JOSUE CASCO HERRERA <djcasco@unah.hn>
2025-02-23 16:23:11 -0600

    cambios en el hooks de actividades

--- src/hooks/useActividades.js
+++ src/hooks/useActividades.js
@@ -1,7 +1,7 @@
 // hooks/useActividades.js
 import { useState, useEffect } from 'react';
 import { datosDePrueba } from '../testeos/MockDataActividadesDisponibles'; // Datos de prueba
-import { fetchData } from '../services/actAPI'; // Importamos la función de fetch de la API
+import fetchAllData from '../services/ActAPI'; // Importamos la función de fetch de la API
 
 export const useActividades = () => {
   const [actividades, setActividades] = useState([]);
@@ -10,11 +10,10 @@ export const useActividades = () => {
   useEffect(() => {
     const cargarActividades = async () => {
       try {
-        const { dataFetch } = await fetchData(); // Intentamos obtener las actividades de la API
-        setActividades(dataFetch.actividades || datosDePrueba.actividades); // Si la API no tiene datos, usamos los de prueba
+        const  dataFetch  = await fetchAllData(); // Intentamos obtener las actividades de la API
+        setActividades(dataFetch.actividades); // Si la API no tiene datos, usamos los de prueba
       } catch (err) {
         console.error("Error al cargar las actividades", err);
-        setActividades(datosDePrueba.actividades); // Si hay un error, usamos los datos de prueba
         setError("No se pudieron cargar las actividades desde la API"); // Guardamos el error si ocurre
       }
     };
