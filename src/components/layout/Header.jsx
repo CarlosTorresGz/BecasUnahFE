35894f3300b04beb038249e09a1022965d75c0bd
npazzety <nelsonpazzety10@gmail.com>
2025-02-24 11:57:14 -0600

    Modalidades

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import '../../styles/Header.css';
 import VOAE from '../../img/VOAE.png';
 import UNAH from '../../img/UNAH.png';
-import { useNavigate, Link } from 'react-router-dom';
 
 const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const navigate = useNavigate();
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+    setIsDropdownOpen(prevState => !prevState);
   };
+
+  const handleClickOutside = (event) => {
+    if (!event.target.closest('.login')) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className="header">
@@ -23,8 +36,8 @@ const Header = () => {
       <nav className="nav">
         <ul className="nav-list">
           <li className="nav-item">Comunicados</li>
-          <li className="nav-item">Becas</li>
-          <li className="nav-item login" onClick={toggleDropdown}>
+          <li className="nav-item" onClick={() => navigate("/tipo-becas")}>Becas</li>
+          <li className="nav-item login" onClick={toggleDropdown} aria-expanded={isDropdownOpen}>
             Login
             <ul className={`dropdown-login ${isDropdownOpen ? 'open' : ''}`}>
               <li onClick={() => navigate("/login")}>Estudiantes</li>
