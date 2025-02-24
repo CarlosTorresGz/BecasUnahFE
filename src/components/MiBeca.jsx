cf9b8f245f2156aea14f300007793d4221fa96ce
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-02-24 11:08:19 -0600

    fix(MiBeca & ProfileBecario) show data

--- src/components/MiBeca.jsx
+++ src/components/MiBeca.jsx
@@ -13,34 +13,56 @@ export const MiBeca = () => {
     const [estadoBeca, setEstadoBeca] = useState(null);
     const [planillas, setPlanillas] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [fechaInicioBeca, setFechaInicioBeca] = useState(null);
 
-    // Obtener datos
-    useEffect(() => {
-        const fetchBecaData = async () => {
-            try {
-                if (user?.beca_id) {
-                    const becaData = await fetchBecaById({ beca_id: user.beca_id });
-                    const becaEstadoData = await fetchStateBecaById({ estado_beca_id: user.estado_beca_id });
-                    if (becaData.state) {
-                        setBeca(becaData.body);
+    const getData = async () => {
+        const userLocal = JSON.parse(localStorage.getItem('user'));
+        console.log('userLocal: ', userLocal);
+
+        const becaId = userLocal ? userLocal.beca_id : null;
+        const estadoBecaId = userLocal ? userLocal.estado_beca_id : null;
+        const becarioId = userLocal ? userLocal.becario_id : null;
+
+        if (!userLocal) {
+            console.error('No se encontró user en localStorage');
+            setError('No se encontró la información del usuario.');
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const becaData = await fetchBecaById({ beca_id: becaId });
+            console.log("API Response:", becaData);
+
+            const planillaData = await fetchPlanillas({ becario_id: becarioId });
+            console.log("API Response:", planillaData);
+
+            if (becaData.state) {
+                setBeca(becaData.body);
+                setFechaInicioBeca(userLocal ? userLocal.fecha_inicio_beca : null)
+                if (estadoBecaId) {
+                    const becaEstadoData = await fetchStateBecaById({ estado_beca_id: estadoBecaId });
+                    console.log("API Response:", becaEstadoData);
+                    if (becaEstadoData.state) {
                         setEstadoBeca(becaEstadoData.body)
                     }
                 }
-
-                if (user?.becario_id) {
-                    const planillaData = await fetchPlanillas({ becario_id: user.becario_id });
-                    if (planillaData.state) {
-                        setPlanillas(planillaData.body);
-                    }
-                }
-            } catch (error) {
-                console.error('Error al obtener los datos:', error);
-            } finally {
-                setLoading(false);
             }
-        };
-        fetchBecaData();
-    }, [user?.beca_id, user?.becario_id, user?.estado_beca_id]);    
+            if (planillaData.state) {
+                setPlanillas(planillaData.body);
+            }
+        } catch (error) {
+            console.error('Error al obtener los datos de la persona:', error);
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     if (loading) {
         return (
@@ -51,13 +73,17 @@ export const MiBeca = () => {
             </div>
         );
     }
+
+    if (error) {
+        return <div>Error al cargar los datos. Por favor, intenta de nuevo más tarde.</div>;
+    }
     return (
         <div className='mi-beca'>
             <div className='mi-beca-informacion'>
                 <h1>Información General</h1>
                 <InfoItem label='Tipo de Beca:' value={beca.nombre_beca} />
-                <InfoItem label='Monto:' value={`L. ${beca.monto.toFixed(2)}`} />
-                <InfoItem label='Fecha de Inicio:' value={user.fecha_inicio_beca} />
+                <InfoItem label='Monto:' value={beca.monto ? `L. ${beca.monto.toFixed(2)}` : 'ND'} />
+                <InfoItem label='Fecha de Inicio:' value={fechaInicioBeca} />
                 <InfoItem label='Estado:' value={estadoBeca} />
             </div>
             <div className='mi-beca-calendario'>
@@ -75,7 +101,7 @@ export const MiBeca = () => {
                             planillas.map((pago, index) => (
                                 <tr key={index}>
                                     <td>{new Date(pago.fecha_planilla).toLocaleString('es-ES', { month: 'long' })}</td>
-                                    <td>L. {beca.monto.toFixed(2)}</td>
+                                    <td>{beca.monto ? `L. {beca.monto.toFixed(2)}` : 'ND'}</td>
                                     <td>{pago.estado_entrega === "Entregado" ? '✅' : '❌'}</td>
                                 </tr>
                             ))
--- src/components/ProfileBecario.jsx
+++ src/components/ProfileBecario.jsx
@@ -16,53 +16,72 @@ export const InfoItem = ({ label, value }) => {
 }
 
 export const ProfileBecario = ({ setActiveComponent }) => {
-    const { user, logout } = useAuth();
+    const { logout } = useAuth();
     const navigate = useNavigate();
     const [persona, setPersona] = useState(null);
     const [carrera, setCarrera] = useState(null)
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [noCuenta, setNoCuenta] = useState(null);
+    const [ultimoAcceso, setUltimoAcceso] = useState(null);
 
     const cerrarSesion = () => {
         logout();
         navigate('/')
     };
 
-    // Obtener datos de la persona al cargar el componente
-    useEffect(() => {
-        const fetchPersonData = async () => {
-            if (user && user?.persona_id && user?.carrera_id) {
-                try {
-                    console.log('Fetching data for persona_id:', user.persona_id);
-                    const personData = await fetchPersonById({ person_id: user.persona_id });
-                    const userCareer = await fetchCareerById({ career_id: user.carrera_id });
-                    
-                    if (personData.state && userCareer.state) {
-                        setPersona(personData.body);
-                        setCarrera(userCareer.body)
-                    }
-                } catch (error) {
-                    console.error('Error al obtener los datos de la persona:', error);
-                    setError(error);
-                } finally {
-                    setLoading(false);
-                }
-            }
-        };
+    const getData = async () => {
+        const userLocal = JSON.parse(localStorage.getItem('user'));
+        console.log('userLocal: ', userLocal);
 
-        if (user !== undefined) {
-            fetchPersonData();
+        const persona_id = userLocal ? userLocal.persona_id : null;
+        const carrera_id = userLocal ? userLocal.carrera_id : null;
+
+        if (!userLocal) {
+            console.error('No se encontró user en localStorage');
+            setError('No se encontró la información del usuario.');
+            setLoading(false);
+            return;
+        }else{
+            console.log(`El ID de la persona es: ${persona_id}`);
+            console.log(`El no_cuenta de la persona es: ${userLocal.no_cuenta}`);
+            console.log(`El ultimo acceso de la persona es: ${userLocal.ultimo_acceso}`);
         }
-    }, [user]);
+        try {
+            const result = await fetchPersonById({ person_id: persona_id });
+            console.log("API Response:", result);
 
-    if (user === undefined || loading || !user) {
+            const userCareer = await fetchCareerById({ career_id: carrera_id });
+            console.log("API Response:", userCareer);
+
+            const personData = result.body;
+
+            if ( result.state && userCareer.state ) {
+                setPersona(personData);
+                setCarrera(userCareer.body)
+                setNoCuenta(userLocal ? userLocal.no_cuenta : null);
+                setUltimoAcceso(userLocal ? userLocal.ultimo_acceso : null);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.error('Error al obtener los datos de la persona:', error);
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    if (loading) {
         return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <Spinner animation="border" role="status" style={{color:"#20527E"}}>
-                <span className="visually-hidden" style={{color:"#20527E"}}>Loading...</span>
-            </Spinner>
-
-        </div>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Spinner animation="border" role="status" style={{ color: "#20527E" }}>
+                    <span className="visually-hidden" style={{ color: "#20527E" }}>Loading...</span>
+                </Spinner>
+            </div>
         );
     }
 
@@ -83,10 +102,10 @@ export const ProfileBecario = ({ setActiveComponent }) => {
             </div>
             <div className="profile-becario-content">
                 <div className="profile-becario-info">
-                    <InfoItem label='No. Cuenta:' value={user.no_cuenta} />
+                    <InfoItem label='No. Cuenta:' value={noCuenta} />
                     <InfoItem label='Correo Institucional:' value={persona.correo_institucional} />
                     <InfoItem label='Carrera:' value={carrera} />
-                    <InfoItem label='Último acceso al sistema:' value={user.ultimo_acceso} />
+                    <InfoItem label='Último acceso al sistema:' value={ultimoAcceso} />
                     <div className="profile-becario-action">
                         <button
                             className='profile-becario-buttom-blue'
