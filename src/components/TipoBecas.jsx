cc43a281f468072465f6f3eb2785c76103bd0635
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-02-24 15:18:51 -0600

    add(modalidadesBecaAPI)

--- src/components/TipoBecas.jsx
+++ src/components/TipoBecas.jsx
@@ -1,25 +1,21 @@
 import { useState, useEffect } from 'react';
 import { Accordion, Form, Button, InputGroup } from 'react-bootstrap';
-import fetchData from '../services/faqAPI'; // Asegúrate de usar el servicio correcto para obtener las becas
+import fetchData from '../services/modalidadesBecaAPI'; // Asegúrate de usar el servicio correcto para obtener las becas
 
 import '../styles/TipoBecas.css';
 
 const TipoBecas = () => {
     const [data, setData] = useState([]);
-    const [originalData, setOriginalData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
 
     const getData = async () => {
         try {
             const result = await fetchData();
-            const preguntas = result.preguntas;
-
-            if (Array.isArray(preguntas)) {
-                console.log("Preguntas recibidas:", preguntas);
-                setData(preguntas);
-                setOriginalData(preguntas);
+            const becas = result.becarios;
+            if (Array.isArray(becas)) {
+                console.log("Becas Recibidas:", becas);
+                setData(becas);
             } else {
-                console.error("Error: La API no devolvió preguntas en el formato esperado.");
+                console.error("Error: La API no devolvió becas en el formato esperado.");
             }
         } catch (error) {
             console.error("Error fetching data:", error);
@@ -30,46 +26,15 @@ const TipoBecas = () => {
         getData();
     }, []);
 
-    const handleSearch = () => {
-        if (searchTerm.trim() === "") {
-            setData(originalData);
-        } else {
-            const filteredData = originalData.filter(item =>
-                item.pregunta.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                item.respuesta.toLowerCase().includes(searchTerm.toLowerCase())
-            );
-            setData(filteredData);
-        }
-    };
-
     return (
-        <div className='faq-section'>
+        <div className='becas-section'>
             <h1>Modalidades de Becas que se te ofrecen:</h1>
-            <InputGroup className="mb-3">
-                <Form.Control
-                    placeholder="Buscar"
-                    aria-label="Buscar"
-                    aria-describedby="basic-addon2"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <Button variant="outline-secondary" id="button-addon2" disabled>
-                    Buscar
-                </Button>
-            </InputGroup>
-
-            {data.filter(item =>
-                item.pregunta.toLowerCase().includes(searchTerm.trim().toLowerCase())
-            ).length > 0 ? (
+            {data.length > 0 ? (
                 <Accordion defaultActiveKey="0">
-                    {data
-                        .filter(item =>
-                            item.pregunta.toLowerCase().includes(searchTerm.trim().toLowerCase())
-                        )
-                        .map((item, index) => (
+                    {data.map((item, index) => (
                             <Accordion.Item eventKey={index.toString()} key={index}>
-                                <Accordion.Header>{item.pregunta}</Accordion.Header>
-                                <Accordion.Body>{item.respuesta}</Accordion.Body>
+                                <Accordion.Header>{item.nombre_beca}</Accordion.Header>
+                                <Accordion.Body>{item.descripcion}</Accordion.Body>
                             </Accordion.Item>
                         ))}
                 </Accordion>
---
+++ src/services/modalidadesBecaAPI.js
@@ -0,0 +1,24 @@
+import apiUrl from "../config";
+
+const fetchData = async () => {
+    try {        
+        const response = await fetch(`${apiUrl}/api/becas?`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+        const dataFetch = await response.json();
+
+        console.log("API Response:", dataFetch);
+
+        return dataFetch;
+    } catch (err) {
+        console.error('Fetch error', err);
+        throw err;
+    }
+
+}
+export default fetchData;
