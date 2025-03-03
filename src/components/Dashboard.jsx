2a8111a2c14b0a90a4059aaf1750bde7f5aa27d8
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-03-03 10:41:32 -0600

    refactor(Dashboard)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,13 +2,14 @@ import '../styles/Dashboard.css';
 import { useState } from 'react';
 import { Sidebar } from './Sidebar';
 import { DropdownMenu } from './DropdownMenuDashboard';
-import { MdEventAvailable, MdCheckCircle, MdDescription, MdSchool, MdPerson, MdEventNote, MdAddTask, MdChecklist, MdHistory, MdEdit, MdLogout } from "react-icons/md";
+import { MdEventAvailable, MdCheckCircle, MdDescription, MdSchool, MdPerson, MdEventNote, MdAddTask, MdChecklist, MdHistory, MdEdit, MdLogout, MdSummarize, MdPeopleAlt, MdAssignmentTurnedIn, MdAssignment, MdReceiptLong } from "react-icons/md";
 import { ProfileBecario } from './ProfileBecario';
 import { MiBeca } from './MiBeca';
 import ActividadesDisponibles from './ActividadesDisponibles';
 import AdminActividades from './AdminActividades';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import 'animate.css';
 
 //Data de las actividades
 import fetchAllData from '../services/ActAPI'
@@ -25,76 +26,129 @@ export const Dashboard = ({ userType }) => {
   const cerrarSesion = () => {
     logout();
     navigate('/');
-};
+  };
 
   const optionBecario = [
     {
-      label: 'Actividades Disponibles',
-      onClick: () => setActiveComponent('Actividades Disponibles'),
-      icon: <MdEventAvailable className="panel-izq-button-icono" />
+      title: 'Actividades',
+      icon: <MdAssignmentTurnedIn className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Disponibles',
+          onClick: () => setActiveComponent('Actividades Disponibles'),
+          icon: <MdEventAvailable className="panel-izq-button-icono" />
+        },
+        {
+          label: 'Inscritas',
+          onClick: () => setActiveComponent('Actividades Disponibles'),
+          icon: <MdAssignment className="panel-izq-button-icono" />
+        },
+        {
+          label: 'Realizadas',
+          onClick: () => setActiveComponent('Actividades Realizadas'),
+          icon: <MdCheckCircle className="panel-izq-button-icono" />
+        },
+      ],
     },
     {
-      label: 'Actividades Realizadas',
-      onClick: () => setActiveComponent('Actividades Realizadas'),
-      icon: <MdCheckCircle className="panel-izq-button-icono" />
+      title: 'Reportes',
+      icon: <MdReceiptLong className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Reportes Recibidos',
+          onClick: () => setActiveComponent('Reportes Recibidos'),
+          icon: <MdDescription className="panel-izq-button-icono" />
+        },
+      ],
     },
     {
-      label: 'Reportes Recibidos',
-      onClick: () => setActiveComponent('Reportes Recibidos'),
-      icon: <MdDescription className="panel-izq-button-icono" />
+      title: 'Perfil',
+      icon: <MdPerson className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Mi Beca',
+          onClick: () => setActiveComponent('Mi Beca'),
+          icon: <MdSchool className="panel-izq-button-icono" />
+        },
+        {
+          label: 'Mi Perfil',
+          onClick: () => setActiveComponent('Bienvenido '),
+          icon: <MdPerson className="panel-izq-button-icono" />
+        },
+      ],
     },
-    {
-      label: 'Mi Beca',
-      onClick: () => setActiveComponent('Mi Beca'),
-      icon: <MdSchool className="panel-izq-button-icono" />
-    },
-    {
-      label: 'Mi Perfil',
-      onClick: () => setActiveComponent('Bienvenido '),
-      icon: <MdPerson className="panel-izq-button-icono" />
-    }
   ];
 
   const optionAdmin = [
     {
-      label: 'Actividades Disponibles',
-      onClick: () => setActiveComponent('Actividades Disponibles'),
-      icon: <MdEventNote className="panel-izq-button-icono" />
+      title: 'Actividades',
+      icon: <MdEventNote  className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Actividades Disponibles',
+          onClick: () => setActiveComponent('Actividades Disponibles'),
+          icon: <MdEventNote className="panel-izq-button-icono" />
+        },
+        {
+          label: 'Nueva Actividad',
+          onClick: () => setActiveComponent('Ingreso de Nueva Actividad'),
+          icon: <MdAddTask className="panel-izq-button-icono" />
+        },
+      ],
     },
     {
-      label: 'Nueva Actividad',
-      onClick: () => setActiveComponent('Ingreso de Nueva Actividad'),
-      icon: <MdAddTask className="panel-izq-button-icono" />
+      title: 'Reportes',
+      icon: <MdSummarize  className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Revisión de Becas',
+          onClick: () => setActiveComponent('Revisión de Becas'),
+          icon: <MdChecklist className="panel-izq-button-icono" />
+        },
+        {
+          label: 'Historial de Reportes',
+          onClick: () => setActiveComponent('Historial de Reportes'),
+          icon: <MdHistory className="panel-izq-button-icono" />
+        },
+      ],
+    },
+
+    {
+      title: 'Gestión de Becarios',
+      icon: <MdPeopleAlt  className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Modificar Becario',
+          onClick: () => setActiveComponent('Modificar Becario'),
+          icon: <MdEdit className="panel-izq-button-icono" />
+        },
+        {
+          label: 'Planilla',
+          onClick: () => setActiveComponent('Planilla'),
+          icon: <MdSummarize  className="panel-izq-button-icono" />
+        },
+      ],
     },
     {
-      label: 'Revisión de Becas',
-      onClick: () => setActiveComponent('Revisión de Becas'),
-      icon: <MdChecklist className="panel-izq-button-icono" />
-    },
-    {
-      label: 'Historial de Reportes',
-      onClick: () => setActiveComponent('Historial de Reportes'),
-      icon: <MdHistory className="panel-izq-button-icono" />
-    },
-    {
-      label: 'Modificar Becario',
-      onClick: () => setActiveComponent('Modificar Becario'),
-      icon: <MdEdit className="panel-izq-button-icono" />
-    },
-    {
-      label: 'Cerrar Sesión',
-      onClick: cerrarSesion,
-      icon: <MdLogout className="panel-izq-button-icono" />
+      title: 'Cerrar Sesión',
+      icon: <MdLogout  className="panel-izq-button-icono" />,
+      clasification: [
+        {
+          label: 'Cerrar Sesión',
+          onClick: cerrarSesion,
+          icon: <MdLogout className="panel-izq-button-icono" />
+        },
+      ],
     }
   ];
 
   const optionSidebar = userType === 'becario' ? optionBecario : userType === 'admin' ? optionAdmin : [];
 
   return (
-    <>
+    <div className='content'>
       <div className='panel-superior'>
         <DropdownMenu optionDropdownMenu={optionSidebar} />
-        <h1>{activeComponent ? activeComponent : "Plataforma Avanzada de Control de Horas PASEE"}</h1>
+        <h1 className='animate__animated animate__bounceIn'>{activeComponent ? activeComponent : "Plataforma Avanzada de Control de Horas PASEE"}</h1>
       </div>
       <div className="principal">
         <Sidebar optionSidebar={optionSidebar} />
@@ -104,16 +158,16 @@ export const Dashboard = ({ userType }) => {
               {(() => {
                 switch (activeComponent) {
                   case 'Actividades Disponibles':
-                    return userType === 'becario' 
-                      ? <ActividadesDisponibles data={dataFetchBecarios.actividades} /> 
+                    return userType === 'becario'
+                      ? <ActividadesDisponibles data={dataFetchBecarios.actividades} />
                       : <AdminActividades data={dataFetch.actividades} />;
                   case 'Bienvenido ':
-                    return <ProfileBecario setActiveComponent={setActiveComponent}/>;
+                    return <ProfileBecario setActiveComponent={setActiveComponent} />;
                   case 'Mi Beca':
                     return <MiBeca />;
                   default:
-                    return userType === 'becario' 
-                      ? <ActividadesDisponibles data={dataFetchBecarios.actividades} /> 
+                    return userType === 'becario'
+                      ? <ActividadesDisponibles data={dataFetchBecarios.actividades} />
                       : <AdminActividades data={dataFetch.actividades} />;
                 }
               })()}
@@ -121,7 +175,7 @@ export const Dashboard = ({ userType }) => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,56 @@
 import { useState } from 'react';
 import { MdMenu } from "react-icons/md";
 import '../styles/Sidebar.css';
+import { FaChevronDown } from "react-icons/fa";
 
-export const Sidebar = ( { optionSidebar } ) => {
+export const Sidebar = ({ optionSidebar }) => {
     const [expanded, setExpanded] = useState(false);
+    const [expandedCategory, setExpandedCategory] = useState({});
+
+    const handleNavigation = () => {
+        setExpanded(!expanded); // Cierra el menú principal
+        /*
+        if (expandedCategory) { // Si las subcategorias estas abiertas las cierra
+            setExpandedCategory(!expandedCategory);
+        }
+        */
+        setExpandedCategory({});
+    };
+
+    const toggleCategory = (index) => {
+        setExpandedCategory((prev) => ({
+            ...prev,
+            [index]: !prev[index], // Alterna solo la categoría clickeada
+        }));
+    };
 
     return (
         <div className={`panel-izq ${expanded ? 'expanded' : ''}`}>
-            <button className="panel-izq-toggle" onClick={() => setExpanded(!expanded)}>
+            <button className="panel-izq-toggle" onClick={handleNavigation}>
                 <MdMenu />
+                {expanded && <span className="panel-izq-button-text">Menú</span>}
             </button>
             {optionSidebar.map((option, index) => (
-                <button className="panel-izq-button" key={index} onClick={option.onClick} title={option.label}>
-                    {option.icon}
-                    {expanded && <span className="panel-izq-button-text">{option.label}</span>}
-                </button>
+                <div key={index} className='panel-izq-category'>
+                    <div className='panel-izq-category-title'>
+                        <button className="panel-izq-toggle" onClick={() => toggleCategory(index)} title={option.title}>
+                            {!expanded && option.icon}
+                            {expanded && <h3 className='panel-izq-button-text' key={index}>{option.title} <FaChevronDown /> </h3>}
+                        </button>
+                    </div>
+                    {expandedCategory[index] && (
+                        <div className='panel-izq-category-clasification'>
+                            {option.clasification.map((clasification, i) => (
+                                <button className="panel-izq-button" key={i} onClick={clasification.onClick} title={clasification.label}>
+                                    {clasification.icon}
+                                    {expanded && <span className="panel-izq-button-text">{clasification.label}</span>}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                </div>
             ))}
+
         </div>
     );
 };
