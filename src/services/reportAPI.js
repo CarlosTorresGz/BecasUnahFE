4d6ef9b0c9692edba1f71c88f3d593dbba87fc27
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-03-06 15:38:47 -0600

    add Report style & fix

--- src/services/reportAPI.js
+++ src/services/reportAPI.js
@@ -16,8 +16,27 @@ export const fetchReport = async ({ no_cuenta }) => {
 
         return { state: true, body: data }
     } catch (error) {
+        return { state: false, body: error };
+    }
+}
+
+export const fetchBecarioInfoReport = async ({ no_cuenta }) => {
+    try {
+        const response = await fetch(`${apiUrl}/api/report/infoBecario/${no_cuenta}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        if (!data) {
+            return { state: false, body: data };
+        }
+
+        return { state: true, body: data }
+    } catch (error) {
         console.error('Error:', error);
         return { state: false, body: error };
     }
-
 }
