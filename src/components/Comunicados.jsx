af0738fde7cc80859b0008a87926555bfba8d3c8
DOUGLAS JOSUE CASCO HERRERA <djcasco@unah.hn>
2025-03-10 01:41:08 -0600

    Implementando Comunicados

--- src/components/Comunicados.jsx
+++ src/components/Comunicados.jsx
@@ -1,90 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Comunicados.css';
 import { FaFileAlt } from 'react-icons/fa';
+import fetchAllData from '../services/afichesData';
 
 const Comunicados = () => {
-  const comunicados = [
-    { nombre: 'Comunicado PASEE NO-3-2024', fecha: '18/10/2024 18:00:01' },
-    { nombre: 'Comunicado PASEE NO-2-2024', fecha: '18/05/2024 18:00:01' },
-    { nombre: 'Aviso-Seguimiento', fecha: '01/02/2024 09:30:01' },
-    { nombre: 'Comunicado-Seguimiento', fecha: '18/01/2024 10:00:01' },
-    { nombre: 'Convocatoria', fecha: '18/05/2024 18:00:01' },
-    { nombre: 'Comunicado PASEE NO-4-2023', fecha: '18/11/2023 15:00:01' },
-  ];
+  const [data, setData] = useState({});
 
-  const convocatorias = [
-    { nombre: 'Comunicado PASEE NO-3-2024', fecha: '18/10/2024 18:00:01' },
-    { nombre: 'Comunicado PASEE NO-2-2024', fecha: '18/05/2024 18:00:01' },
-    { nombre: 'Aviso-Seguimiento', fecha: '01/02/2024 09:30:01' },
-    { nombre: 'Comunicado-Seguimiento', fecha: '18/01/2024 10:00:01' },
-    { nombre: 'Comunicado PASEE NO-4-2023', fecha: '18/11/2023 15:00:01' },
-  ];
+  useEffect(() => {
+    // Llamar la función fetchAllData para obtener los datos
+    const fetchData = async () => {
+      try {
+        const fetchedData = await fetchAllData(); 
+        console.log('Fetched Data:', fetchedData);
 
-  const circulares = [
-    { nombre: 'Circular-VOAE-No-001', fecha: '17/01/2024 14:12:01' },
-  ];
+        const groupedDataByCategory = {};
+
+        // Recorre las claves de la respuesta y agrupa los documentos por categoría
+        Object.keys(fetchedData).forEach((key) => {
+          const categoryItems = fetchedData[key];
+          categoryItems.forEach(item => {
+            const categoria = item.nombre_categoria;
+            if (!groupedDataByCategory[categoria]) {
+              groupedDataByCategory[categoria] = [];
+            }
+            groupedDataByCategory[categoria].push(item);
+          });
+        });
+
+        // Establecer el estado con los datos agrupados
+        setData(groupedDataByCategory);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="comunicados-container">
-      <h2>Comunicados</h2>
-      <table className="comunicados-table">
-        <thead>
-          <tr>
-            <th>Nombre del Documento</th>
-            <th>Información</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-          {comunicados.map((comunicado, index) => (
-            <tr key={index}>
-              <td><FaFileAlt /> {comunicado.nombre}</td>
-              <td>{comunicado.fecha}</td>
-              <td><button className="download-button">Descargar</button></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h2>Convocatoria</h2>
-      <table className="convocatoria-table">
-        <thead>
-          <tr>
-            <th>Nombre del Documento</th>
-            <th>Información</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-          {convocatorias.map((convocatoria, index) => (
-            <tr key={index}>
-              <td><FaFileAlt /> {convocatoria.nombre}</td>
-              <td>{convocatoria.fecha}</td>
-              <td><button className="download-button">Descargar</button></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h2>Circulares</h2>
-      <table className="circulares-table">
-        <thead>
-          <tr>
-            <th>Nombre del Documento</th>
-            <th>Información</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-          {circulares.map((circular, index) => (
-            <tr key={index}>
-              <td><FaFileAlt /> {circular.nombre}</td>
-              <td>{circular.fecha}</td>
-              <td><button className="download-button">Descargar</button></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {/* Recorre las categorías y genera una tabla por cada una */}
+      {Object.entries(data).map(([categoria, documentos], index) => (
+        documentos.length > 0 && (
+          <div key={index}>
+            <h2>{categoria}</h2>
+            <table className="comunicados-table">
+              <thead>
+                <tr>
+                  <th>Nombre del Documento</th>
+                  <th>Información</th>
+                  <th>Enlace</th>
+                </tr>
+              </thead>
+              <tbody>
+                {documentos.map((documento, idx) => (
+                  <tr key={idx}>
+                    <td><FaFileAlt /> {documento.nombre_afiche}</td>
+                    <td>{documento.fecha_actividad}</td>
+                    <td>
+                      <a href={documento.url_afiche} 
+                        className="download-button" target="_blank" 
+                        rel="noopener noreferrer">
+                        Descargar
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )
+      ))}
     </div>
   );
 };
