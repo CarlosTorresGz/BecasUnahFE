40b7681c479154c6a379678bd901b24544c293f1
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-03-10 10:31:11 -0600

    add(participantesActividadAPI)

--- src/components/ListadoAsistencia.jsx
+++ src/components/ListadoAsistencia.jsx
@@ -1,118 +1,159 @@
 import React, { useState, useEffect } from "react";
-import { Card, Table, Button, ListGroup, Form, Row, Col } from "react-bootstrap";
-import { jsPDF } from "jspdf";
+import { Card, Table, Button, ListGroup, Form, Row, Col, InputGroup } from "react-bootstrap";
 import "../styles/ListadoAsistencia.css"
-
-const becarios = {
-    A00000: [
-        { id: 101, nombre: "Juan Pérez" },
-        { id: 102, nombre: "Ana Gómez" },
-    ],
-    A00001: [
-        { id: 201, nombre: "Carlos Rodríguez" },
-        { id: 202, nombre: "María López" },
-    ],
-    A00002: [
-        { id: 301, nombre: "Luis Fernández" },
-        { id: 302, nombre: "Sofía Méndez" },
-    ],
-};
+import generatePDF from "../services/listGenerator";
+import { fetchParticipantesActividadById, actualizarAsistencia } from "../services/participantesActividadAPI";
+import { toast } from "sonner";
+import { MdSearch } from "react-icons/md";
 
 const ListadoAsistencia = ({ data }) => {
-    const [actividades, setActividades] = useState(data);
-    const [actividadSeleccionada, setActividadSeleccionada] = useState(1);
-    const [asistencia, setAsistencia] = useState({});
+    const [actividadSeleccionada, setActividadSeleccionada] = useState(data[0].actividad_id);
+    const [participantesActividad, setParticipantesActividad] = useState([]);
+    const [error, setError] = useState('');
+    const [searchTerm, setSearchTerm] = useState("");
 
-    const toggleAsistencia = (id) => {
-        setAsistencia((prev) => ({ ...prev, [id]: !prev[id] }));
-    };
+    const obtenerParticipantesActividad = async (actividad_id) => {
+        setActividadSeleccionada(actividad_id);
 
-    // Cargar el estado de la asistencia desde localStorage cuando se monta el componente
-    useEffect(() => {
-        const storedAsistencia = JSON.parse(localStorage.getItem("asistencia"));
-        if (storedAsistencia) {
-            setAsistencia(storedAsistencia);
+        const response = await fetchParticipantesActividadById({ actividad_id });
+
+        if (response.state) {
+            setParticipantesActividad(response.body);
+        } else {
+            setParticipantesActividad([]);
+            setError(response.body.error);
         }
-    }, []);
+    }
 
-    // Guardar el estado de la asistencia en localStorage cada vez que cambie
     useEffect(() => {
-        if (Object.keys(asistencia).length > 0) {
-            localStorage.setItem("asistencia", JSON.stringify(asistencia));
+        obtenerParticipantesActividad(actividadSeleccionada);
+    }, [actividadSeleccionada]);
+
+    const toggleAsistencia = async (actividad_id, no_cuenta) => {
+        try {
+            const asistenciaActualizada = await actualizarAsistencia({ actividadId: actividad_id, noCuenta: no_cuenta });
+
+            // Actualizar el estado en el frontend
+            setParticipantesActividad((prev) =>
+                prev.map((becario) =>
+                    becario["No. Cuenta"] === no_cuenta
+                        ? { ...becario, Asistencia: !becario.Asistencia }
+                        : becario
+                )
+            );
+            
+            participantesActividad.map((becario) => {
+                if (becario["No. Cuenta"] === no_cuenta) {
+                    if (!becario.Asistencia) {
+                        toast.success(`${asistenciaActualizada.body}`);
+                    }else {
+                        toast.warning(`Asistencia desmarcada.`);
+                    }
+                }
+            });
+            
+        } catch (error) {
+            console.error("Error al actualizar la asistencia:", error);
         }
-    }, [asistencia]);
-
-    const descargarLista = () => {
-        const doc = new jsPDF();
-        doc.text(`Lista de Asistencia - ${actividades.find(a => a.actividad_id === actividadSeleccionada).nombre_actividad}`, 10, 10);
-        becarios[actividadSeleccionada].forEach((b, index) => {
-            doc.text(`${index + 1}. ${b.nombre} - ${asistencia[b.id] ? "Asistió" : "No asistió"}`, 10, 20 + index * 10);
-        });
-
-        const nombreArchivo = `lista_asistencia_${actividades.find(a => a.actividad_id === actividadSeleccionada).nombre_actividad}.pdf`;
-        doc.save(nombreArchivo);
     };
 
     return (
         <div className="p-4">
             <Row>
                 {/* Card de Actividades */}
-                <Col md={6} className="mb-4">
+                <Col md={5} className="mb-4">
                     <Card>
                         <Card.Body>
                             <Card.Title>Actividades</Card.Title>
+                            <InputGroup className="mb-3">
+                                <Form.Control
+                                    placeholder="Buscar"
+                                    aria-label="Buscar"
+                                    aria-describedby="basic-addon2"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                />
+                                <Button variant="outline-secondary" id="button-addon2">
+                                    <MdSearch />
+                                </Button>
+                            </InputGroup>
                             <div className="scrollable-card">
-                                <ListGroup>
-                                    {actividades.map((actividad) => (
-                                        <ListGroup.Item
-                                            key={actividad.actividad_id}
-                                            action
-                                            active={actividadSeleccionada === actividad.actividad_id}
-                                            onClick={() => setActividadSeleccionada(actividad.actividad_id)}
-                                        >
-                                            {actividad.nombre_actividad}
-                                        </ListGroup.Item>
-                                    ))}
-                                </ListGroup>
+                                {data.filter(item =>
+                                    item.nombre_actividad.toLowerCase().includes(searchTerm.trim().toLowerCase())
+                                ).length > 0 ? (
+                                    <ListGroup>
+                                        {data.filter(item =>
+                                            item.nombre_actividad.toLowerCase().includes(searchTerm.trim().toLowerCase())
+                                        )
+                                            .map((actividad) => (
+                                                <ListGroup.Item
+                                                    key={actividad.actividad_id}
+                                                    action
+                                                    active={actividadSeleccionada === actividad.actividad_id}
+                                                    onClick={() => {
+                                                        /*setActividadSeleccionada(actividad.actividad_id);
+                                                        obtenerParticipantesActividad(actividadSeleccionada);*/
+                                                        obtenerParticipantesActividad(actividad.actividad_id);
+                                                    }
+                                                    }
+                                                >
+                                                    {actividad.nombre_actividad}
+                                                </ListGroup.Item>
+                                            ))}
+                                    </ListGroup>
+                                ) : (
+                                    <p className="no-results">No se encontraron resultados</p>
+                                )}
                             </div>
                         </Card.Body>
                     </Card>
                 </Col>
 
                 {/* Card de Lista de Asistencia */}
-                <Col md={6} className="mb-4">
+                <Col md={7} className="mb-4">
                     <Card>
                         <Card.Body>
                             <Card.Title>Lista de Asistencia</Card.Title>
                             <div className="scrollable-card">
                                 <Table striped bordered hover>
-                                    <thead>
+                                    <thead className="table-thead">
                                         <tr>
+                                            <th>No. Cuenta</th>
                                             <th>Becario</th>
+                                            <th>Correo Institucional</th>
                                             <th>Asistencia</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        {becarios[actividadSeleccionada]?.map((becario) => (
-                                            <tr key={becario.id}>
-                                                <td>{becario.nombre}</td>
-                                                <td className="text-center">
-                                                    <Form.Check
-                                                        type="checkbox"
-                                                        checked={asistencia[becario.id] || false}
-                                                        onChange={() => toggleAsistencia(becario.id)}
-                                                    />
-                                                </td>
+                                    <tbody className="table-tbody">
+                                        {participantesActividad && participantesActividad.length > 0 ? (
+                                            participantesActividad.map((becario) => (
+                                                <tr key={becario["No. Cuenta"]}>
+                                                    <td>{becario["No. Cuenta"]}</td>
+                                                    <td style={{ textAlign: 'left' }}>{becario["Nombre Completo"]}</td>
+                                                    <td style={{ textAlign: 'left' }}>{becario["Correo Institucional"]}</td>
+                                                    <td className="text-center">
+                                                        <Form.Check
+                                                            type="checkbox"
+                                                            checked={becario.Asistencia}
+                                                            onChange={() => toggleAsistencia(actividadSeleccionada, becario["No. Cuenta"])}
+                                                        />
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        ) : (
+                                            <tr>
+                                                <td colSpan={4} className="text-center">{error}</td>
                                             </tr>
-                                        ))}
+                                        )}
                                     </tbody>
                                 </Table>
-                                <Button variant="primary" onClick={descargarLista}>
-                                    Descargar Lista
-                                </Button>
+
                             </div>
                         </Card.Body>
                     </Card>
+                    <Button variant="primary" className="button-asistencia mt-3" onClick={() => generatePDF(participantesActividad, data.find(a => a.actividad_id === actividadSeleccionada).nombre_actividad)}>
+                        Descargar Lista
+                    </Button>
                 </Col>
             </Row>
         </div >
