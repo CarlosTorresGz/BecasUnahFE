9c9f9dae1e95440645137a5d1d0533cc9d977e82
Rodrigo Funes <rodrigofunes@hotmail.es>
2025-03-21 00:26:41 -0600

    refactor

---
+++ src/util/uploadPictureAzure.js
@@ -0,0 +1,28 @@
+import { BlobServiceClient, AnonymousCredential } from '@azure/storage-blob';
+
+const AZURE_STORAGE_ACCOUNT_NAME = 'storageprojectunah';
+const containerName = 'contenedorpictures';
+const sasToken = 'sp=racw&st=2025-02-24T19:51:49Z&se=2025-04-25T03:51:49Z&sv=2022-11-02&sr=c&sig=EM79r3Wzz%2FuTP9LBmy0eplE6rs7vhnzdaL84Hq2vBB4%3D';  // Reemplaza con tu token SAS
+
+export const uploadImageToAzure = async (file, nombreActividad) => {
+    const blobServiceClient = new BlobServiceClient(
+        `https://${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net?${sasToken}`,
+        new AnonymousCredential()
+    );
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobName = `${nombreActividad}-${Date.now()}`;
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    try {
+        await blockBlobClient.uploadData(file, {
+            blobHTTPHeaders: {
+                blobContentType: file.type
+            }
+        });
+        console.log('url: ', blockBlobClient.url)
+        return blockBlobClient.url;
+    } catch (error) {
+        console.error('Error al subir la imagen:', error.message);
+        throw error;
+    }
+};
