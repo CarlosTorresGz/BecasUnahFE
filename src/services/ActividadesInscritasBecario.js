358b299e2d25d1cc6821df7191edf7f28f1f973d
Douglas Casco <herreradouglas919@gmail.com>
2025-03-24 00:03:38 -0600

    implementando apis de actividades de becario

---
+++ src/services/ActividadesInscritasBecario.js
@@ -0,0 +1,33 @@
+import apiUrl from "../config";
+import { useAuth } from '../context/AuthContext';
+
+export const ActividadesInscritasData = async (no_cuenta) => {
+    try {
+        // Obtén el número de cuenta del localStorage o del contexto de autenticación
+       console.log("el numero de cuenta es: ",no_cuenta)
+        // Verificar si el número de cuenta está disponible
+        if (!no_cuenta) {
+            throw new Error("Número de cuenta no encontrado");
+        }
+         //const cuenta = no_cuenta   // Envía la solicitud POST con el número de cuenta
+        const response = await fetch(`${apiUrl}/api/postActivityInProgressByAccount?`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({no_cuenta}), // Aquí pasas el número de cuenta como parte del cuerpo
+        });
+
+        if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+
+        const Data = await response.json();
+        console.log(Data)
+        return Data;
+
+    } catch (err) {
+        console.error('Fetch error', err);
+        throw err;
+    }
+}
+
+export default ActividadesInscritasData;
