f969dda0f8eacbf8e982da2aae7ee33242f555a7
Douglas Casco <herreradouglas919@gmail.com>
2025-04-08 01:10:57 -0600

    agregando boton de eliminar para FAQ

---
+++ src/services/FAQ/EliminarPregunta.js
@@ -0,0 +1,28 @@
+import apiUrl from "../../config";
+
+export const handleDelete = async ({ empleado_id, pregunta_id }) => {
+  try {
+    const token = localStorage.getItem('jwtToken');
+    if (!token) {
+        console.warn('No se encontró token JWT');
+        return { state: false, body: 'Autenticación requerida' };
+    }
+
+    const response = await fetch(`${apiUrl}/api/DeleteFAQ?`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({ empleado_id, actividad_id })
+    });
+
+    if (!response.ok) {
+      throw new Error('No se pudo eliminar la pregunta');
+    }
+
+    return { state: true }
+  } catch (error) {
+    return { state: false, body: error }
+  }
+};
